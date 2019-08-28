dfb56cc412bd106c5872437d98eaf28162d8c7a4
{dp7jm} <{dpjm@chef.net}>
2019-08-29 00:41:01 +0200

    Add project files.

---
+++ StopWatchFirst/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatchFirst
+{
+    class StopWatch
+    {
+        private TimeSpan _timeSpan = new TimeSpan();
+        private DateTime _dateTime = new DateTime();
+        private bool _counting = false;
+        public void Start()
+        {
+            if (!_counting)
+            {
+
+            }
+
+
+        }
+        public void Stop()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
